f9aac653e4b259303a55433dd83426034350a95d
Sulabh <sulabh4@hotmail.com>
2018-04-28 10:01:31 +0545

    Development: Added Interval Detection

---
+++ PolarCycle/Interval.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace PolarCycle {
+    partial class Interval {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.intervalGrid = new MetroFramework.Controls.MetroGrid();
+            this.intervalPanel = new MetroFramework.Controls.MetroPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.intervalGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // intervalGrid
+            // 
+            this.intervalGrid.AllowUserToAddRows = false;
+            this.intervalGrid.AllowUserToDeleteRows = false;
+            this.intervalGrid.AllowUserToResizeRows = false;
+            this.intervalGrid.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.intervalGrid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.intervalGrid.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.intervalGrid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.intervalGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.intervalGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.intervalGrid.DefaultCellStyle = dataGridViewCellStyle2;
+            this.intervalGrid.EnableHeadersVisualStyles = false;
+            this.intervalGrid.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.intervalGrid.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.intervalGrid.Location = new System.Drawing.Point(23, 63);
+            this.intervalGrid.Name = "intervalGrid";
+            this.intervalGrid.ReadOnly = true;
+            this.intervalGrid.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.intervalGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.intervalGrid.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.intervalGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.intervalGrid.Size = new System.Drawing.Size(1044, 599);
+            this.intervalGrid.TabIndex = 0;
+            // 
+            // intervalPanel
+            // 
+            this.intervalPanel.AutoScroll = true;
+            this.intervalPanel.HorizontalScrollbar = true;
+            this.intervalPanel.HorizontalScrollbarBarColor = true;
+            this.intervalPanel.HorizontalScrollbarHighlightOnWheel = false;
+            this.intervalPanel.HorizontalScrollbarSize = 10;
+            this.intervalPanel.Location = new System.Drawing.Point(1082, 63);
+            this.intervalPanel.Name = "intervalPanel";
+            this.intervalPanel.Size = new System.Drawing.Size(245, 599);
+            this.intervalPanel.TabIndex = 1;
+            this.intervalPanel.VerticalScrollbar = true;
+            this.intervalPanel.VerticalScrollbarBarColor = true;
+            this.intervalPanel.VerticalScrollbarHighlightOnWheel = false;
+            this.intervalPanel.VerticalScrollbarSize = 10;
+            // 
+            // Interval
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1366, 740);
+            this.Controls.Add(this.intervalPanel);
+            this.Controls.Add(this.intervalGrid);
+            this.Name = "Interval";
+            this.Text = "Interval";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Interval_FormClosed);
+            this.Load += new System.EventHandler(this.Interval_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.intervalGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroGrid intervalGrid;
+        private MetroFramework.Controls.MetroPanel intervalPanel;
+    }
+}
