e7beff9ad74aed8b8216881b7209d668838bc4c8
Sulabh <tsulabh4@gmail.com>
2018-05-28 13:33:28 +0545

    Add Interval Files

---
+++ NewFile/Interval/Interval.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace PolarCycle {
+    partial class Interval {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.intervalGrid = new MetroFramework.Controls.MetroGrid();
+            this.intervalPanel = new MetroFramework.Controls.MetroPanel();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            ((System.ComponentModel.ISupportInitialize)(this.intervalGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // intervalGrid
+            // 
+            this.intervalGrid.AllowUserToAddRows = false;
+            this.intervalGrid.AllowUserToDeleteRows = false;
+            this.intervalGrid.AllowUserToResizeRows = false;
+            this.intervalGrid.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.intervalGrid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.intervalGrid.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.intervalGrid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.intervalGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            this.intervalGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.intervalGrid.DefaultCellStyle = dataGridViewCellStyle8;
+            this.intervalGrid.EnableHeadersVisualStyles = false;
+            this.intervalGrid.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.intervalGrid.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.intervalGrid.Location = new System.Drawing.Point(23, 63);
+            this.intervalGrid.Name = "intervalGrid";
+            this.intervalGrid.ReadOnly = true;
+            this.intervalGrid.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.intervalGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle9;
+            this.intervalGrid.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.intervalGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.intervalGrid.Size = new System.Drawing.Size(1044, 599);
+            this.intervalGrid.TabIndex = 0;
+            // 
+            // intervalPanel
+            // 
+            this.intervalPanel.AutoScroll = true;
+            this.intervalPanel.HorizontalScrollbar = true;
+            this.intervalPanel.HorizontalScrollbarBarColor = true;
+            this.intervalPanel.HorizontalScrollbarHighlightOnWheel = false;
+            this.intervalPanel.HorizontalScrollbarSize = 10;
+            this.intervalPanel.Location = new System.Drawing.Point(1082, 63);
+            this.intervalPanel.Name = "intervalPanel";
+            this.intervalPanel.Size = new System.Drawing.Size(245, 599);
+            this.intervalPanel.TabIndex = 1;
+            this.intervalPanel.VerticalScrollbar = true;
+            this.intervalPanel.VerticalScrollbarBarColor = true;
+            this.intervalPanel.VerticalScrollbarHighlightOnWheel = false;
+            this.intervalPanel.VerticalScrollbarSize = 10;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(112, 25);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(98, 23);
+            this.metroButton1.TabIndex = 2;
+            this.metroButton1.Text = "NoisyIntervals";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(216, 25);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(109, 23);
+            this.metroButton2.TabIndex = 3;
+            this.metroButton2.Text = "ComplexIntervals";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Location = new System.Drawing.Point(331, 25);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(104, 23);
+            this.metroButton3.TabIndex = 4;
+            this.metroButton3.Text = "OriginalIntervals";
+            this.metroButton3.UseSelectable = true;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // Interval
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1366, 740);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.intervalPanel);
+            this.Controls.Add(this.intervalGrid);
+            this.Name = "Interval";
+            this.Text = "Interval";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Interval_FormClosed);
+            this.Load += new System.EventHandler(this.Interval_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.intervalGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroGrid intervalGrid;
+        private MetroFramework.Controls.MetroPanel intervalPanel;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton3;
+    }
+}
---
+++ NewFile/Interval/Interval.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using MetroFramework;
+
+namespace PolarCycle {
+    public partial class Interval : MetroForm {
+        string[] columnNames = new string[] { "Heart Rate", "Speed", "Cadence", "Altitude", "Power", "Time" };
+        double thresholdPower;
+        double ftp;
+        Dictionary<string, string> param;
+        string IntervalData;
+        List<Data> DataList;
+
+        public Interval() {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets all the information from Graph
+        /// </summary>
+        /// <param name="param">Dictionary to be used</param>
+        /// <param name="IntervalData">The Interval Data</param>
+        /// <param name="DataList">DataList of the corresponding filename</param>
+        public void getInformation(Dictionary<string, string> param, string IntervalData, List<Data> DataList, double threshold, double ftp) {
+            this.param = param;
+            this.IntervalData = IntervalData;
+            this.DataList = DataList;
+            this.thresholdPower = threshold;
+            this.ftp = ftp;
+            Console.WriteLine("Check Interval Here");
+            Console.WriteLine("Param");
+            Console.WriteLine(param);
+            Console.WriteLine("IntervalData");
+            Console.WriteLine(IntervalData);
+            Console.WriteLine("DataList");
+            Console.WriteLine(DataList);
+        }
+
+        private void Interval_Load(object sender, EventArgs e) {
+            IntervalDetect();
+        }
+
+        /// <summary>
+        /// Checks the Unit
+        /// </summary>
+        /// <returns>Boolean Value</returns>
+        private bool isEuroUnitUsed() {
+            bool isEuroUnit = false;
+
+            switch (param["Version"]) {
+                case "105": {
+                        isEuroUnit = (param["Mode"][2] == '0');
+                    }
+                    break;
+
+                case "106":
+                case "107": {
+                        isEuroUnit = (param["SMode"][7] == '0');
+                    }
+                    break;
+            }
+
+            return isEuroUnit;
+        }
+
+        /// <summary>
+        /// Function to Detect Interval
+        /// </summary>
+        private void IntervalDetect() {
+            DateTime dtime;
+            DataTable dt = new DataTable();
+            //checking speed units
+            string speedUnit = (isEuroUnitUsed() ? " km/h" : " mph");
+            //checking altitude units
+            string altitudeUnit = (isEuroUnitUsed() ? " meters" : " feet");
+            //getting date value from header
+            string date = param["Date"];
+            Console.WriteLine("Date");
+            Console.WriteLine(date);
+            //getting time value from header
+            string[] timesArr = param["StartTime"].Split(':');
+            //getting year from date
+            int year = Convert.ToInt32(date.Substring(0, 4));
+            //getting month from date
+            int month = Convert.ToInt32(date.Substring(4, 2));
+            //getting day from date
+            int day = Convert.ToInt32(date.Substring(6, 2));
+            //getting hour from timearr
+            int hour = Convert.ToInt32(timesArr[0]);
+            //getting minutes from timearr
+            int minutes = Convert.ToInt32(timesArr[1]);
+            //getting seconds from timearr
+            int seconds = (int)Convert.ToDouble(timesArr[2]);
+
+            dtime = new DateTime(year, month, day, hour, minutes, seconds);
+
+            int i = 0;
+            int startIndex = 0;
+            int counter = 0;
+            int numIntervals = 0;
+            int interval = int.Parse(IntervalData);
+
+            Console.WriteLine("Interval Data Here");
+            Console.WriteLine(interval);
+
+            List<Data> filtered;
+            intervalPanel.Controls.Clear();
+            //adding interval columns in datagrid view
+            dt.Columns.Add("Interval");
+
+            foreach (string col in columnNames) {
+                dt.Columns.Add(col);
+            }
+
+            //when input power is greter than thresoldpower
+            foreach (Data hr in DataList) {
+                if (hr.power > thresholdPower) {
+                    if (counter == 0) {
+                        startIndex = i;
+                    }
+                    counter++;
+                } else {
+                    if (counter >= 10) {
+                        numIntervals++;
+                        filtered = new List<Data>();
+
+                        for (int j = startIndex; j < i; j++) {
+                            Data hrd = new Data();
+
+                            hrd.heart_rate = DataList[j].heart_rate;
+                            hrd.speed = DataList[j].speed;
+                            hrd.cadence = DataList[j].cadence;
+                            hrd.altitude = DataList[j].altitude;
+                            hrd.power = DataList[j].power;
+
+                            filtered.Add(hrd);
+
+                            DataRow dr = dt.NewRow();
+                            //for interval
+                            dr[0] = "Interval " + numIntervals;
+                            //for heart rate
+                            dr[1] = hrd.heart_rate;
+                            //for speed
+                            dr[2] = hrd.speed;
+                            //for cadence
+                            dr[3] = hrd.cadence;
+                            //for altitude
+                            dr[4] = hrd.altitude;
+                            //for power
+                            dr[5] = hrd.power;
+                            //for time
+                            dr[6] = dtime.AddSeconds(j * interval).ToLongTimeString();
+
+                            dt.Rows.Add(dr);
+                        }
+
+                        System.Windows.Forms.Label headingLabel = new System.Windows.Forms.Label();
+
+                        headingLabel.AutoSize = true;
+                        headingLabel.Font = new System.Drawing.Font("trebuchet ms", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                        headingLabel.Name = "intervalHeading" + numIntervals;
+                        headingLabel.Text = "Interval " + numIntervals + ":";
+                        headingLabel.Location = new Point(0, (numIntervals - 1) * 175);
+
+                        System.Windows.Forms.Label detailsLabel = new System.Windows.Forms.Label();
+
+                        //passing all intervals information to detailsLabel 
+                        detailsLabel.AutoSize = true;
+                        detailsLabel.Font = new System.Drawing.Font("calibri", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                        detailsLabel.Name = "detailsInterval" + numIntervals;
+                        detailsLabel.Text = "Average Heart Rate: \t" + Math.Round(filtered.Average(data => data.heart_rate)) + " bpm \n"
+                                            + "Average Speed: \t" + Math.Round(filtered.Average(data => data.speed)) + speedUnit + " \n"
+                                            + "Average Cadence: \t" + Math.Round(filtered.Average(data => data.cadence)) + " \n"
+                                            + "Average Altitude: \t" + Math.Round(filtered.Average(data => data.altitude)) + altitudeUnit + " \n"
+                                            + "Average Power: \t" + Math.Round(filtered.Average(data => data.power)) + " watts \n";
+
+                        detailsLabel.Location = new Point(0, 25 + (numIntervals - 1) * 175);
+
+                        intervalPanel.Controls.Add(headingLabel);
+                        intervalPanel.Controls.Add(detailsLabel);
+                    }
+                    counter = 0;
+                }
+                i++;
+            }
+            //adding data into intervalsGrird
+            intervalGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            intervalGrid.DataSource = dt;
+        }
+
+        private void IntervalDetectWithParams(int num) {
+            DateTime dtime;
+            DataTable dt = new DataTable();
+            //checking speed units
+            string speedUnit = (isEuroUnitUsed() ? " km/h" : " mph");
+            //checking altitude units
+            string altitudeUnit = (isEuroUnitUsed() ? " meters" : " feet");
+            //getting date value from header
+            string date = param["Date"];
+            Console.WriteLine("Date");
+            Console.WriteLine(date);
+            //getting time value from header
+            string[] timesArr = param["StartTime"].Split(':');
+            //getting year from date
+            int year = Convert.ToInt32(date.Substring(0, 4));
+            //getting month from date
+            int month = Convert.ToInt32(date.Substring(4, 2));
+            //getting day from date
+            int day = Convert.ToInt32(date.Substring(6, 2));
+            //getting hour from timearr
+            int hour = Convert.ToInt32(timesArr[0]);
+            //getting minutes from timearr
+            int minutes = Convert.ToInt32(timesArr[1]);
+            //getting seconds from timearr
+            int seconds = (int)Convert.ToDouble(timesArr[2]);
+
+            dtime = new DateTime(year, month, day, hour, minutes, seconds);
+
+            int i = 0;
+            int startIndex = 0;
+            int counter = 0;
+            int numIntervals = 0;
+            int interval = int.Parse(IntervalData);
+
+            Console.WriteLine("Interval Data Here");
+            Console.WriteLine(interval);
+
+            List<Data> filtered;
+            intervalPanel.Controls.Clear();
+            //adding interval columns in datagrid view
+            dt.Columns.Add("Interval");
+
+            foreach (string col in columnNames) {
+                dt.Columns.Add(col);
+            }
+
+            //when input power is greter than thresoldpower
+            foreach (Data hr in DataList) {
+                if (hr.power > thresholdPower) {
+                    if (counter == 0) {
+                        startIndex = i;
+                    }
+                    counter++;
+                } else {
+                    if (counter >= num) {
+                        numIntervals++;
+                        filtered = new List<Data>();
+
+                        for (int j = startIndex; j < i; j++) {
+                            Data hrd = new Data();
+
+                            hrd.heart_rate = DataList[j].heart_rate;
+                            hrd.speed = DataList[j].speed;
+                            hrd.cadence = DataList[j].cadence;
+                            hrd.altitude = DataList[j].altitude;
+                            hrd.power = DataList[j].power;
+
+                            filtered.Add(hrd);
+
+                            DataRow dr = dt.NewRow();
+                            //for interval
+                            dr[0] = "Interval " + numIntervals;
+                            //for heart rate
+                            dr[1] = hrd.heart_rate;
+                            //for speed
+                            dr[2] = hrd.speed;
+                            //for cadence
+                            dr[3] = hrd.cadence;
+                            //for altitude
+                            dr[4] = hrd.altitude;
+                            //for power
+                            dr[5] = hrd.power;
+                            //for time
+                            dr[6] = dtime.AddSeconds(j * interval).ToLongTimeString();
+
+                            dt.Rows.Add(dr);
+                        }
+
+                        System.Windows.Forms.Label headingLabel = new System.Windows.Forms.Label();
+
+                        headingLabel.AutoSize = true;
+                        headingLabel.Font = new System.Drawing.Font("trebuchet ms", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                        headingLabel.Name = "intervalHeading" + numIntervals;
+                        headingLabel.Text = "Interval " + numIntervals + ":";
+                        headingLabel.Location = new Point(0, (numIntervals - 1) * 175);
+
+                        System.Windows.Forms.Label detailsLabel = new System.Windows.Forms.Label();
+
+                        //passing all intervals information to detailsLabel 
+                        detailsLabel.AutoSize = true;
+                        detailsLabel.Font = new System.Drawing.Font("calibri", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                        detailsLabel.Name = "detailsInterval" + numIntervals;
+                        detailsLabel.Text = "Average Heart Rate: \t" + Math.Round(filtered.Average(data => data.heart_rate)) + " bpm \n"
+                                            + "Average Speed: \t" + Math.Round(filtered.Average(data => data.speed)) + speedUnit + " \n"
+                                            + "Average Cadence: \t" + Math.Round(filtered.Average(data => data.cadence)) + " \n"
+                                            + "Average Altitude: \t" + Math.Round(filtered.Average(data => data.altitude)) + altitudeUnit + " \n"
+                                            + "Average Power: \t" + Math.Round(filtered.Average(data => data.power)) + " watts \n";
+
+                        detailsLabel.Location = new Point(0, 25 + (numIntervals - 1) * 175);
+
+                        intervalPanel.Controls.Add(headingLabel);
+                        intervalPanel.Controls.Add(detailsLabel);
+                    }
+                    counter = 0;
+                }
+                i++;
+            }
+            //adding data into intervalsGrird
+            intervalGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            intervalGrid.DataSource = dt;
+        }
+
+        private void Interval_FormClosed(object sender, FormClosedEventArgs e) {
+            Application.Exit();
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e) {
+            Random random = new Random();
+            int num = random.Next(1, 10);
+            IntervalDetectWithParams(num);
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e) {
+            //double num = ftp + 0.1;
+            double num = (ftp + 0.1)/100;
+            //double num = ftp * 0.1;
+            int numCasted = 20 * Convert.ToInt32(num);
+            Console.WriteLine("NumCasted");
+            Console.WriteLine(numCasted);
+            IntervalDetectWithParams(numCasted);
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e) {
+            IntervalDetect();
+        }
+    }
+}
